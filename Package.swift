5c138918faf8c41ae26cbb09ca03c240dccca2de
Adam <adam.rotmil@gmail.com>
2026-03-18 12:17:49 -0400

    Create Package.swift for InteractionLab
    Initialize Swift package with iOS and macOS support.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift Package Manager required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "InteractionLab",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "InteractionLab",
+            targets: ["InteractionLab"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "InteractionLab",
+            path: "InteractionLab"
+        )
+    ]
+)
